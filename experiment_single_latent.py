0aa28ca082f7912afe98db57091d7cded7ed97f0
Ivan Puhachov <ivan.puhachov@gmail.com>
2024-03-13 20:58:26 -0400

    upd

--- experiment_single_latent.py
+++ experiment_single_latent.py
@@ -10,59 +10,51 @@ from nerf.sd import StableDiffusion, seed_everything
 from torch.optim.lr_scheduler import LambdaLR
 
 import matplotlib.pyplot as plt
-
-def get_cosine_schedule_with_warmup(optimizer, num_warmup_steps, num_training_steps, num_cycles: float = 0.5):
-
-    def lr_lambda(current_step):
-        if current_step < num_warmup_steps:
-            return float(current_step) / float(max(1, num_warmup_steps))
-        progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
-        return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
-
-    return LambdaLR(optimizer, lr_lambda, -1)
+from experiment_single_rgb import get_cosine_schedule_with_warmup
 
 
-device = 'cuda:0'
-guidance = StableDiffusion(device)
-guidance.vae.encoder = None
+if __name__ == '__main__':
+    device = 'cuda:0'
+    guidance = StableDiffusion(device)
+    guidance.vae.encoder = None
 
-prompt = 'pineapple'
-text_embeddings = guidance.get_text_embeds(prompt, '')
-guidance.text_encoder.to('cpu')
-torch.cuda.empty_cache()
+    prompt = 'pineapple'
+    text_embeddings = guidance.get_text_embeds(prompt, '')
+    guidance.text_encoder.to('cpu')
+    torch.cuda.empty_cache()
 
-seed_everything(42)
-latents = nn.Parameter(torch.randn(1, 4, 64, 64, device=device))
-optimizer = torch.optim.AdamW([latents], lr=1e-1, weight_decay=0)
-num_steps = 1000
-scheduler = get_cosine_schedule_with_warmup(optimizer, 100, int(num_steps*1.5))
+    seed_everything(42)
+    latents = nn.Parameter(torch.randn(1, 4, 64, 64, device=device))
+    optimizer = torch.optim.AdamW([latents], lr=1e-1, weight_decay=0)
+    num_steps = 1000
+    scheduler = get_cosine_schedule_with_warmup(optimizer, 100, int(num_steps*1.5))
 
-for step in tqdm(range(num_steps)):
-    optimizer.zero_grad()
+    for step in tqdm(range(num_steps)):
+        optimizer.zero_grad()
 
-    t = torch.randint(guidance.min_step, guidance.max_step + 1, [1], dtype=torch.long, device=guidance.device)
-    with torch.no_grad():
-        # add noise
-        noise = torch.randn_like(latents)
-        latents_noisy = guidance.scheduler.add_noise(latents, noise, t)
-        # pred noise
-        latent_model_input = torch.cat([latents_noisy] * 2)
-        noise_pred = guidance.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+        t = torch.randint(guidance.min_step, guidance.max_step + 1, [1], dtype=torch.long, device=guidance.device)
+        with torch.no_grad():
+            # add noise
+            noise = torch.randn_like(latents)
+            latents_noisy = guidance.scheduler.add_noise(latents, noise, t)
+            # pred noise
+            latent_model_input = torch.cat([latents_noisy] * 2)
+            noise_pred = guidance.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
 
-    # perform guidance (high scale from paper!)
-    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-    noise_pred = noise_pred_uncond + 100 * (noise_pred_text - noise_pred_uncond)
+        # perform guidance (high scale from paper!)
+        noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+        noise_pred = noise_pred_uncond + 100 * (noise_pred_text - noise_pred_uncond)
 
-    w = (1 - guidance.alphas[t])
-    grad = w * (noise_pred - noise)
+        w = (1 - guidance.alphas[t])
+        grad = w * (noise_pred - noise)
 
-    latents.backward(gradient=grad, retain_graph=True)
+        latents.backward(gradient=grad, retain_graph=True)
 
-    optimizer.step()
-    scheduler.step()
+        optimizer.step()
+        scheduler.step()
 
-    if step > 0 and step % 100 == 0:
-        rgb = guidance.decode_latents(latents)
-        img = rgb.detach().squeeze(0).permute(1,2,0).cpu().numpy()
-        print('[INFO] save image', img.shape, img.min(), img.max())
-        plt.imsave(f'tmp_lat_img_{step}.jpg', img)
+        if step > 0 and step % 100 == 0:
+            rgb = guidance.decode_latents(latents)
+            img = rgb.detach().squeeze(0).permute(1,2,0).cpu().numpy()
+            print('[INFO] save image', img.shape, img.min(), img.max())
+            plt.imsave(f'tmp_lat_img_{step}.jpg', img)
--- experiment_single_rgb.py
+++ experiment_single_rgb.py
@@ -11,58 +11,51 @@ from torch.optim.lr_scheduler import LambdaLR
 
 import matplotlib.pyplot as plt
 
-def get_cosine_schedule_with_warmup(optimizer, num_warmup_steps, num_training_steps, num_cycles: float = 0.5):
 
+def get_cosine_schedule_with_warmup(
+        opt,
+        num_warmup_steps,
+        num_training_steps,
+        num_cycles: float = 0.5,
+):
     def lr_lambda(current_step):
         if current_step < num_warmup_steps:
             return float(current_step) / float(max(1, num_warmup_steps))
         progress = float(current_step - num_warmup_steps) / float(max(1, num_training_steps - num_warmup_steps))
         return max(0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)))
 
-    return LambdaLR(optimizer, lr_lambda, -1)
+    return LambdaLR(opt, lr_lambda, -1)
 
 
-device = 'cuda:0'
-guidance = StableDiffusion(device)
-guidance.vae.encoder = None
+if __name__ == '__main__':
+    device = 'cuda:0'
+    guidance = StableDiffusion(device)
+    # limited memory here, don't need the decoder
+    guidance.vae.decoder = None
 
-prompt = 'pineapple'
-text_embeddings = guidance.get_text_embeds(prompt, '')
-guidance.text_encoder.to('cpu')
-torch.cuda.empty_cache()
+    prompt = 'pineapples'
+    text_embeddings = guidance.get_text_embeds(prompt, '')
+    guidance.text_encoder.to('cpu')
+    torch.cuda.empty_cache()
 
-seed_everything(42)
-latents = nn.Parameter(torch.randn(1, 4, 64, 64, device=device))
-optimizer = torch.optim.AdamW([latents], lr=1e-1, weight_decay=0)
-num_steps = 1000
-scheduler = get_cosine_schedule_with_warmup(optimizer, 100, int(num_steps*1.5))
+    seed_everything(42)
+    # put parameters approximately in range(0, 1) since this is what `encode_imgs` expects
+    rgb = nn.Parameter(torch.randn(1, 3, 512, 512, device=device) / 2 + .5)
+    optimizer = torch.optim.AdamW([rgb], lr=1e-1, weight_decay=0)
+    num_steps = 5000
+    scheduler = get_cosine_schedule_with_warmup(
+        opt=optimizer,
+        num_warmup_steps=100,
+        num_training_steps=int(num_steps * 1.5),
+    )
 
-for step in tqdm(range(num_steps)):
-    optimizer.zero_grad()
+    for step in tqdm(range(num_steps)):
+        optimizer.zero_grad()
+        guidance.train_step(text_embeddings, rgb, guidance_scale=100)
 
-    t = torch.randint(guidance.min_step, guidance.max_step + 1, [1], dtype=torch.long, device=guidance.device)
-    with torch.no_grad():
-        # add noise
-        noise = torch.randn_like(latents)
-        latents_noisy = guidance.scheduler.add_noise(latents, noise, t)
-        # pred noise
-        latent_model_input = torch.cat([latents_noisy] * 2)
-        noise_pred = guidance.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+        optimizer.step()
+        scheduler.step()
 
-    # perform guidance (high scale from paper!)
-    noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-    noise_pred = noise_pred_uncond + 100 * (noise_pred_text - noise_pred_uncond)
-
-    w = (1 - guidance.alphas[t])
-    grad = w * (noise_pred - noise)
-
-    latents.backward(gradient=grad, retain_graph=True)
-
-    optimizer.step()
-    scheduler.step()
-
-    if step > 0 and step % 100 == 0:
-        rgb = guidance.decode_latents(latents)
-        img = rgb.detach().squeeze(0).permute(1,2,0).cpu().numpy()
-        print('[INFO] save image', img.shape, img.min(), img.max())
-        plt.imsave(f'tmp_lat_img_{step}.jpg', img)
+    plt.imshow(rgb.detach().clamp(0, 1).squeeze(0).permute(1, 2, 0).cpu())
+    plt.axis('off')
+    plt.show()
