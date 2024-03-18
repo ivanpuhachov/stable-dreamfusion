6df5f7f9db255bcb0b43c4ad66d380a6fa2e1534
Ivan <ivan.puhachov@gmail.com>
2024-03-18 17:14:50 -0400

    trying to optimize sds loss

--- experiment_2d_implicit.py
+++ experiment_2d_implicit.py
@@ -4,6 +4,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os
 from tqdm import tqdm
+from nerf.sd import StableDiffusion, seed_everything
+from experiment_single_rgb import get_cosine_schedule_with_warmup
 
 
 class Implicit2D(torch.nn.Module):
@@ -16,7 +18,9 @@ class Implicit2D(torch.nn.Module):
             torch.nn.Linear(n_hidden_neurons, n_hidden_neurons),
             torch.nn.Softplus(beta=10.0),
             torch.nn.Linear(n_hidden_neurons, 3),
-            torch.nn.Sigmoid(),
+            # torch.nn.Softplus(beta=10.0),
+            # torch.nn.Linear(n_hidden_neurons, 3),
+            # torch.nn.Sigmoid(),
             # To ensure that the colors correctly range between [0-1],
             # the layer is terminated with a sigmoid layer.
         )
@@ -30,6 +34,7 @@ class Implicit2D(torch.nn.Module):
         x, y = torch.meshgrid(xs, ys, indexing='xy')
         xy = torch.stack([x, y], dim=2).view(-1, 2).to(device)
         rgb = self.forward(xy).view(size, size, 3)
+        # rgb = torch.clip(rgb, 0.0, 1.0)
         return rgb
 
 
@@ -52,7 +57,7 @@ class Implicit2DTrainer:
             save_all=True,
             append_images=frames[1:],
             optimize=False,
-            duration=5,
+            duration=5 * len(frames) / 20,
             loop=0,
         )
 
@@ -82,6 +87,45 @@ class Implicit2DTrainer:
         plt.figure()
         plt.imshow(frames[-1])
         plt.show()
+    
+    def optimize_sds(
+            self,
+            prompt="pineapple",
+            n_steps=500,
+            save_every=5,
+    ):
+        frames = []
+        guidance = StableDiffusion(self.device)
+        guidance.vae.decoder = None
+        text_embeddings = guidance.get_text_embeds(prompt, '')
+        guidance.text_encoder.to('cpu')
+        torch.cuda.empty_cache()
+        seed_everything(42)
+        # rgb = torch.nn.Parameter(torch.rand(1, 3, 128, 128, device=self.device) / 2 + .5)
+        # optimizer = torch.optim.Adam(self.model.parameters(), lr=0.1)
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=1e-3, weight_decay=0)
+        # optimizer = torch.optim.Adam([rgb], lr=0.01)
+        # optimizer = torch.optim.AdamW([rgb], lr=1e-1, weight_decay=0)
+        scheduler = get_cosine_schedule_with_warmup(
+            opt=optimizer,
+            num_warmup_steps=100,
+            num_training_steps=int(n_steps * 1.5),
+        )
+        for i_step in (pbar := tqdm(range(n_steps))):
+            optimizer.zero_grad()
+            output = self.model.render(size=128)
+            guidance.train_step(text_embeddings, output.unsqueeze(0).permute(0,3,1,2), guidance_scale=100)
+            # guidance.train_step(text_embeddings, rgb, guidance_scale=100)
+            optimizer.step()
+            scheduler.step()
+            # rgb.data = rgb.data.clip(0, 1)
+            if (i_step % save_every == (save_every-1)) or (i_step == 0) or (i_step == n_steps):
+                frames.append((output.detach().cpu().numpy() * 255).astype(np.uint8))
+                # frames.append((rgb.squeeze(0).permute(1,2,0).detach().cpu().numpy() * 255).astype(np.uint8))
+                Image.fromarray(frames[-1]).save(
+                    f"last.png",
+                )
+        self.save_frames_as_gif("demo.gif", frames)
 
 
 def test_implicit_2d():
@@ -97,7 +141,8 @@ def test_implicit_2d_trainer():
     gt_image[height // 2:, width // 2:, :] = torch.tensor([0.0, 0.0, 1.0])
     mymodel = Implicit2D()
     mytrainer = Implicit2DTrainer(model=mymodel)
-    mytrainer.fit_image(image=gt_image, n_steps=800, save_every=20)
+    # mytrainer.fit_image(image=gt_image, n_steps=800, save_every=20)
+    mytrainer.optimize_sds()
 
 
 if __name__ == '__main__':
